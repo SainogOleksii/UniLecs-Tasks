7163c7f684f7a1622848a8ebda98b07df89fe210
SainogOleksii <sainog.o.m@gmail.com>
2017-11-21 03:37:32 +0200

    Task 43_2

--- Task43/src/Task43.java
+++ Task43/src/Task43.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Task43 {
     public static void main (String args[]){
-        System.out.println("e");
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        int c = cin.nextInt();
+        Box box_1 = new Box(a, b, c);
+        a = cin.nextInt();
+        b = cin.nextInt();
+        c = cin.nextInt();
+        Box box_2 = new Box(a, b, c);
+        if(box_1.fitIn(box_2) && box_2.fitIn(box_1))
+            System.out.println("Boxes are equal");
+        else  if(box_1.fitIn(box_2))
+                System.out.println("The first box is smaller than the second one");
+              else  if(box_1.fitIn(box_2))
+                System.out.println("The first box is smaller than the second one");
+                    else if(box_2.fitIn(box_1))
+                            System.out.println("The first box is larger than the second one");
+                         else
+                             System.out.println("Boxes are incomparable");
     }
 }
