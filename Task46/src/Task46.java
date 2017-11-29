726d386cb60aa7c7db4e80046efdd53a233c89fb
SainogOleksii <sainog.o.m@gmail.com>
2017-11-29 17:21:50 +0200

    Task46

---
+++ Task46/src/Task46.java
@@ -0,0 +1,9 @@
+public class Task46 {
+    private static int revers(int num, int res){
+        return (num == 0)? res: revers(num / 10, res * 10 + num % 10);
+    }
+
+    public static void main(String args[]){
+        System.out.println(revers(1234, 0));
+    }
+}
